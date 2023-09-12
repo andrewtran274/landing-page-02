2c2711f51f22b553a691e7960a94e406c7a42f2f
andrewtran274 <andrewtran274@gmail.com>
2023-09-12 23:06:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./Components/Sidebar";
+
+const App = () => {
+  return (
+    <div className="Main">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default App;
